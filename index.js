7c85493bdf316c1bd219ce14320b88c813d99901
cristianpb <felipebrokate@gmail.com>
2019-08-31 19:35:17 +0200

    Add postgres data base route

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const express = require('express')
+const { Client } = require('pg')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const client = new Client({
+  host: 'localhost',
+  port: 5432,
+  user: 'postgres',
+  password: 'secret_passwd',
+  database: 'postgres',
+})
+
+app.get('/', async (req, res) => {
+  await client.connect()
+  const result = await client.query('SELECT * FROM data2')
+  console.log(result.rows)
+  res.status(200).send(result.rows);
+  await client.end()
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
