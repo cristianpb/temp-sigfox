bd249b72f76121a73f9fe0a344c9f7f6fa8713dc
cristianpb <felipebrokate@gmail.com>
2019-10-09 23:47:55 +0200

    Update micropython code for DS18B20 probe

--- micropython/boot.py
+++ micropython/boot.py
@@ -1,13 +1,14 @@
 import gc
 import esp
 import uos
-import dht
+import ubinascii
 from utime import sleep
 from machine import UART, Pin, DEEPSLEEP, RTC, deepsleep
+from ds18x20 import DS18X20
+from onewire import OneWire
 
 esp.osdebug(None)       # turn off vendor O/S debugging messages
 gc.collect()
-sensor = dht.DHT22(Pin(5))
 
 
 class Sigfox(object):
@@ -24,41 +25,64 @@ class Sigfox(object):
             timeout: timeout in seconds
         """
         iter_count = timeout / 0.1
+        success_h = ubinascii.hexlify(success).decode('utf-8')
+        failure_h = ubinascii.hexlify(failure).decode('utf-8')
         message = ""
 
-        while (iter_count >= 0 and success not in message and failure not in message):
+        while (iter_count >= 0 and
+                success_h not in message and
+                failure_h not in message):
             if self.uart.any() > 0:
                 c = self.uart.read()
-                message += c.decode("utf-8")
+                hex_str = ubinascii.hexlify(c).decode("utf-8")
+                message += hex_str
             iter_count -= 1
             sleep(0.1)
-        if success in message:
+        if success_h in message:
             return message
-        elif failure in message:
+        elif failure_h in message:
             print("Failure ({})".format(message.replace('\r\n', '')))
         else:
             print("Received timeout ({})".format(message.replace('\r\n', '')))
         return ''
 
-    def test_connection(self):
-        print("Testing connection")
-        self.uart.write(b"AT\r")
-        if self.wait_for('OK', 'ERROR', 3):
-            print("connection ok")
+    def test_connection(self, timeout=3):
+        print('testing connection')
+        self.uart.write("AT\r")
+        if self.wait_for('OK', 'ERROR', timeout):
+            return 'connection ok'
+        else:
+            return ''
 
     def send_message(self, message):
-        self.test_connection()
-        print("Sending message")
-        self.uart.write(b"AT$SF={}\r".format(message))
+        res = ''
+        while not res:
+            res = self.test_connection(timeout=1)
+        print('connection ok, sending message')
+        self.uart.write("AT$SF={}\r".format(message))
         if self.wait_for('OK', 'ERROR', 15):
-            print("Message send")
+            print('Message send')
 
 
-def temp_humidity(captor):
-    captor.measure()
-    temp = captor.temperature()
-    humidity = captor.humidity()
-    return temp, humidity
+class TemperatureSensor:
+    def __init__(self, pin):
+        """Finds address of single DS18B20 on bus specified by `pin`
+        Args:
+            pin: 1-Wire bus pin
+        """
+        self.ds = DS18X20(OneWire(Pin(pin)))
+        addrs = self.ds.scan()
+        if not addrs:
+            raise Exception('no DS18B20 found at bus on pin %d' % pin)
+        # save what should be the only address found
+        self.addr = addrs.pop()
+
+    def read_temp(self):
+        """Reads temperature from a single DS18X20
+        """
+        self.ds.convert_temp()
+        sleep(0.75)
+        return self.ds.read_temp(self.addr)
 
 
 def deep_sleep(msecs):
@@ -73,9 +97,9 @@ def deep_sleep(msecs):
     deepsleep()
 
 
-temp, humidity = temp_humidity(sensor)
 sfx = Sigfox()
-sfx.send_message("{:.2f}{:.2f}".format(temp, humidity).replace(".", ""))
+sensor = TemperatureSensor(5)
+temp = sensor.read_temp()
+sfx.send_message("{:.2f}".format(temp).replace(".", ""))
 
-# sleep for 10 seconds (10000 milliseconds)
 deep_sleep(900000)
